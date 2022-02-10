e3f9f0ed856167de7727246272ef9f6fedee9af4
João Amador <nnettoolliveira@gmail.com>
2022-02-10 02:52:45 -0300

    melhorando o visual com chalk

---
+++ 2_FUNDAMENTOS/8_chalk/index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+const nota = 6;
+
+if (nota >= 7) {
+  console.log(chalk.green.bold("Parabéns, você passou!"));
+} else {
+  console.log(chalk.bgRed.black("Você precisa fazer a prova final!"));
+}
