ea9f986d3d691e54460d6a0d7fa1ca6805c1028b
João Amador <nnettoolliveira@gmail.com>
2022-02-24 18:30:03 -0300

    atualizando a função de deposito

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -83,7 +83,7 @@ function deposit() {
     inquirer.prompt([
         {
             name: 'accountName',
-            message: 'Qual o nome da sua conta'
+            message: 'Qual o nome da sua conta?'
         }
     ]).then((answer) => {
         const accountName = answer['accountName'];
@@ -92,6 +92,21 @@ function deposit() {
         if (!checkAccount(accountName)) {
             return deposit();
         }
+
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'Quanto você deseja depositar?'
+            }
+        ]).then((answer) => {
+            const amount = answer['amount'];
+
+            // add an amount
+            addAmount(accountName, amount);
+            operation();
+
+        }).catch(err => console.log(err));
+
     }).catch((err) => {
         console.log(err);
     })
@@ -104,4 +119,33 @@ function checkAccount(accountName) {
     }
 
     return true;
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName);
+
+    if (!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+        return deposit();
+    }
+
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData), function (err) {
+            console.log(err);
+        }
+    );
+
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta`));
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r'
+    });
+
+    return JSON.parse(accountJSON);
 }
