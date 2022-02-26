e083c34f40284eb5eb782ee7cbd8257dd4534ff0
João Amador <nnettoolliveira@gmail.com>
2022-02-25 22:50:31 -0300

    adicionando metodo para visualizar o saldo da conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -28,6 +28,8 @@ function operation() {
                 createAccount();
                 break;
             case 'Consultar Saldo':
+                getAccountBalance();
+                break;
             case 'Depositar':
                 deposit();
                 break;
@@ -148,4 +150,26 @@ function getAccount(accountName) {
     });
 
     return JSON.parse(accountJSON);
+}
+
+// show account balance
+function getAccountBalance() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ]).then((answer) => {
+        const accountName = answer['accountName'];
+
+        // verify if account exists
+        if (!checkAccount(accountName)) {
+            return getAccountBalance();
+        }
+
+        const accountData = getAccount(accountName);
+
+        console.log(chalk.bgBlue.black(`Olá, o saldo da sua conta é de R$${accountData.balance}`));
+        operation();
+    }).catch(err => console.log(err));
 }
