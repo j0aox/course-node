1e572841044c65fb92ead51b53f440405c6f21fc
João Amador <nnettoolliveira@gmail.com>
2022-03-09 08:44:34 -0300

    como utilizar css com node

---
+++ 6_EXPRESS/9_colocando_css/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+
+const users  = require('./users/index');
+
+// ler o body
+app.use(
+    express.urlencoded({
+    extended: true,
+    })
+);
+
+app.use(express.json());
+
+// arquivos estáticos
+app.use(express.static('public'));
+
+const basePath = path.join(__dirname, 'templates');
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
