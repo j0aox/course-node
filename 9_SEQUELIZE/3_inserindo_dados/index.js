b520cea3fe4c0d331f571057d28f1fac2e5ae84d
João Amador <nnettoolliveira@gmail.com>
2022-04-08 07:45:26 -0300

    adicionando dados

---
+++ 9_SEQUELIZE/3_inserindo_dados/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const con = require("./db/conn");
+
+const User = require("./models/User");
+
+const app = express();
+
+// configurando para pegar os dados do body como json
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+// configurando para pegar os dados do body como json
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/users/create", (req, res) => {
+    res.render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;
+
+    if (newsletter === 'on') {
+        newsletter = true;
+    } else {
+        newsletter = false;
+    }
+
+    console.log(req.body);
+
+    await User.create({
+        name,
+        occupation,
+        newsletter,
+    });
+
+    res.redirect("/");
+})
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+con
+    .sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Servidor rodando na porta http://localhost:3000");
+        });
+    })
+    .catch((err) => console.log(err));
