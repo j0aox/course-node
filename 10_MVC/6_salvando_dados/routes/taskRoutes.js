eb8f334e5d740cecc75404fba964cb370820e430
João Amador <nnettoolliveira@gmail.com>
2022-07-01 10:48:57 -0300

    salvando os dados da requisição na base de dados

---
+++ 10_MVC/6_salvando_dados/routes/taskRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.showTasks);
+router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave);
+
+module.exports = router;
