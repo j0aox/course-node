5b494b82931b8ef64b21a939d94dab4569d6b2a9
João Amador <nnettoolliveira@gmail.com>
2022-07-04 16:46:51 -0300

    salvando os dados no db

---
+++ 10_MVC/10_dados_atualizado/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.showTasks);
+router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave);
+router.post('/remove', TaskController.removeTask);
+router.get('/edit/:id', TaskController.updateTask);
+router.post('/edit', TaskController.updateTaskPost);
+
+module.exports = router;
