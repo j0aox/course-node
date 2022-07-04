860766eec19668562fc5676d8a75d4347e43159a
João Amador <nnettoolliveira@gmail.com>
2022-07-04 11:30:38 -0300

    removendo dados

---
+++ 10_MVC/8_deletando_dados/routes/taskRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.showTasks);
+router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave);
+router.post('/remove', TaskController.removeTask);
+
+module.exports = router;
